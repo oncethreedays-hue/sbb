cbff18120d2a8d9882acd93574fae058091f0553
user <user@DESKTOP-GQE7VMF>
2026-07-26 11:17:43 +0900

    Question Service, Answer Service, Detail Page

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -11,7 +11,12 @@ public class MainController {
 	@GetMapping("/sbb")
 	@ResponseBody
 	public String index( ) {
-		return "index";
+		return "안녕하세요 sbb에 오신것을 환영합니다.";
+	}
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";
 	}
 	
 
---
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,33 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
+
+import groovyjarjarantlr4.v4.parse.ANTLRParser.throwsSpec_return;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+	
+	private final QuestionRepository questionRepository;
+	
+	public List<Question> getList() {
+		return this.questionRepository.findAll();
+	}
+	
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		
+		if(question.isPresent()) {
+			return question.get();			
+		} else {
+			throw new DataNotFoundException("quetions not found");
+		}
+	}
+
+}
