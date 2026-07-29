79fbfe871ee97540f7c24586f19d44818a36a5ff
user <user@DESKTOP-GQE7VMF>
2026-07-29 17:17:55 +0900

    3-08

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,15 +1,19 @@
 package com.mysite.sbb.answer;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,21 +25,26 @@ public class AnswerController {
 	
 	private final QuestionService questionService;
 	private final AnswerService answerService;
+	private final UserService userService;
 	
+	@PreAuthorize("isAuthenticated()")
 	@PostMapping("/create/{id}")
 	public String createAnswer(
 			Model model, 
 			@PathVariable("id") Integer id, 
 			@Valid AnswerForm answerForm, 
-			BindingResult bindingResult) {
+			BindingResult bindingResult,
+			Principal pricipal) {
 		
 		Question question = this.questionService.getQuestion(id);
+		SiteUser siteUser = this.userService.getUser(pricipal.getName());
+		
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("question", question);
 			return "question_detail";
 			
 		} else {
-			this.answerService.create(question, answerForm.getContent());
+			this.answerService.create(question, answerForm.getContent(), siteUser);
 		}
 		return String.format("redirect:/question/detail/%s", id);
 		
