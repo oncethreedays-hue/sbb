793710bc5dc80285bed9a5e46cfd6a969d3e0318
user <user@DESKTOP-GQE7VMF>
2026-07-30 17:11:14 +0900

    3-13

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
@@ -13,4 +14,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	Question findBySubjectAndContent(String subject, String Content);
 	List<Question> findBySubjectLike(String subject);
 	Page<Question> findAll(Pageable pageable);
+	Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
